ab8e3921f61d4f3c7f9807ffe89857c34e974cfc
meritback <merit.back@online.de>
2023-02-15 13:42:40 +0000

    untested model for lp approach

--- sparsecca/_multicca_pmd.py
+++ sparsecca/_multicca_pmd.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.linalg import svd
+import pyomo as pyo
 
 from ._utils_pmd import binary_search, l2n, soft, scale
 
@@ -28,10 +29,31 @@ def update_w(datasets, idx, sumabs, ws, ws_final):
         w_ = soft(tots, sumabs) / l2n(soft(tots, sumabs))
 
         return w_
+    
+
+# Linear Programming 
+
+def ObjRule(model):
+    """Objective Function (4.3 in witten 2009)"""
+    return sum((model.w[model.X[i]].T @ model.X[i].T  @ model.X[j] @ model.w[model.X[j]]) for i in range(model.X) for j in range(model.X) if i<j )
+
+def lasso(model, xi):
+    # sum over all entries of vector wi (i in [1:K])
+    return sum(abs(model.w[xi][f]) for f in range(len(model.w[xi])) ) <= model.c[xi]
+
+def norm2(model, xi):
+    # sum over all entries of vector wi (i in [1:K])
+    return sum(model.w[xi][f] * model.w[xi][f] for f in range(len(model.w[xi])) <= 1)
+
+
+def init_w(xi):
+    # TODO use svd vh from scipy 
+    return [0] * xi.shape[1]
+
 
 
 def multicca(datasets, penalties, niter=25, K=1, standardize=True, mimic_R=True):
-    """Re-implementation of the MultiCCA function from R package PMA.
+    """Re-implementation of the MultiCCA Using Linear programming.
 
     Params
     ------
@@ -52,6 +74,66 @@ def multicca(datasets, penalties, niter=25, K=1, standardize=True, mimic_R=True)
         List of arrays of shape (datasets.shape[1], K) corresponding to the
         sparse canonical variates per dataset.
     """
+    # preprocessing:
+    datasets = datasets.copy()
+    for data in datasets:
+        if data.shape[1] < 2:
+            raise Exception('Need at least 2 features in each dataset')
+
+    if standardize:
+        for idx in range(len(datasets)):
+            if mimic_R:
+                datasets[idx] = scale(datasets[idx], center=True, scale=True)
+            else:
+                datasets[idx] = scale(datasets[idx], center=True, scale=False)
+
+
+
+
+    # Linear Programming
+    model = pyo.ConcreteModel()
+
+    # set: Xi i in [1:K]
+    #Xi = set(x for x in datasets if x)
+    model.X = pyo.Set(initialize=datasets) 
+
+    # params: ci i in [1:K]
+    model.c = pyo.Param(model.X, initialize=penalties)
+
+    # variables: wi i in [1:K]
+    #each wi needs to be a vector 1*len(features) -> features is amount of columns in Xi use list 
+    
+    model.w = pyo.Var(model.X, initialize=init_w)
+
+    # Objective
+    model.Obj = pyo.Objective(rule=ObjRule, sense=pyo.maximize)
+
+    # constraints: lasso 
+    model.constraint_lasso = pyo.ConstraintList()
+    model.constraint_lasso.add(model.X, rule=lasso) 
+    """for xi in model.X:
+        #lasso
+        model.constraint_lasso.add(sum(model.w[xi][f] for f in range(len(model.w[xi])) <= model.c[xi]))
+    """
+    # constraints: (2-norm)^2 ||wi||22 <=1
+    model.constraint_norm2 = pyo.ConstraintList()
+    model.constraint_norm2.add(model.X, rule=norm2)
+    """for xi in model.X:
+        #norm2
+        model.constraint_norm2.add(sum(model.w[xi][f] * model.w[xi][f] for f in range(len(model.w[xi])) <= 1))
+    """
+
+    opt = pyo.SolverFactory('glpk')
+    instance = model.create_instance()
+    res = opt.solve(instance)
+
+    model.solutions.load_from(res)
+    weights = [wi for wi in instance.w]
+
+    return weights
+
+
+    # OLD CODE
     datasets = datasets.copy()
     for data in datasets:
         if data.shape[1] < 2:
