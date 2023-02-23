c3fe0db45de093eb711ed06d662c1756e618c437
Yuge Ji <jiyuge@gmail.com>
2023-02-23 19:50:22 +0100

    clean up docstrings and loops

--- sparsecca/_multicca_lp.py
+++ sparsecca/_multicca_lp.py
@@ -1,10 +1,9 @@
 import numpy as np
-import pandas as pd
 from scipy.linalg import svd
 from collections import defaultdict
 import pyomo.environ as pyo
 
-from ._utils_pmd import binary_search, l2n, soft, scale   
+from ._utils_pmd import scale
 
 
 def preprocess_datasets(datasets:list, standardize=True, mimic_R=True):
@@ -15,7 +14,7 @@ def preprocess_datasets(datasets:list, standardize=True, mimic_R=True):
         if len(data[0]) < 2:
             raise Exception('Need at least 2 features in each dataset')
 
-        # standardize if set TRUE
+    # standardize if set TRUE
     if standardize:
         for idx in range(len(datasets)):
             if mimic_R:
@@ -41,15 +40,17 @@ def ObjRule(model):
 
 
 def _update_w_lp(datasets, penalties, ws_init):
-    """ solves 4.3 of witten 2009 with linear programming approach
-        -------
-        Parameters:  
-            datasets: N matrices [samples x features]
-            penalties: list of length N for each Xi
-        
-        -------
-        Returns: 
-            w: defaultdict(list)
+    """Solves 4.3 of witten 2009 with linear programming approach
+
+    Params
+    ------
+    datasets: N matrices [samples x features]
+    penalties: list of length N for each Xi
+    ws_init: initial values for ws, usually provided by svd
+
+    Returns
+    -------
+        w: defaultdict(list)
             - for each matrix Xn in datasets (n in [1:n]): n-> weights_vector
             - each weights_vector: list of length f (featuresize)
             - f = len(datasets[0][0])
@@ -102,53 +103,57 @@ def _update_w_lp(datasets, penalties, ws_init):
 
 
 
-def lp_pmd(datasets:list, penalties:list,  K:int, standardize:bool, mimic_R):
-    """ calculates K weights [1xN]
+def lp_pmd(datasets, penalties, K=1, standardize=True, mimic_R=True):
+    """Calculates K weights [1xN]
+
+    Params
+    ------
+    datasets : list
+        List of n matrices of shape (samples x features)
+    penalties : list
+        List of n (1 x features) vectors. `c` in Witten 2009
+    K : int (default: 1)
+        Number of latent factors to calculate.
+    standardize : bool (default: True)
+        Whether to center and scale each dataset before computing sparse
+        canonical variates.
+    mimic_R : bool (default: True)
+        Whether to mimic the R implementation exactly. Note that this flag can
+        significantly change the resulting values.
+
+    Returns
     -------
-    Parameters:  
-        datasets: N matrices [samples x features]
-        penalties: list of length N for each Xi
-        K: Amount of MCPs
-    
-    -------
-    Returns: weights
-        w_final : list
+    w_final : list
         - list of length N, arrays of shape feature x K
-        w_init: initialized with 0.5
+    w_init: initialized with 0.5
     """
     sample_size = len(datasets[0])
-    feature_amount = len(datasets[0][0])
+    n_features = len(datasets[0][0])
     
     datasets_next = preprocess_datasets(datasets, standardize=standardize, mimic_R=mimic_R)
     weights = []
     
     k = 0
-    while k < K:
+    for k in range(K):
         ws_init=[]
         for idx in range(len(datasets_next)):
             ws_init.append(svd(datasets_next[idx])[2][0:K].T)
-
-    
         w = _update_w_lp(datasets_next, penalties, ws_init)
         datasets_current = datasets_next
     
         w_samples = {}
         for w_n in w:
-            w_sample = np.repeat(w[w_n],sample_size, axis=0).reshape(sample_size,feature_amount)
+            w_sample = np.repeat(w[w_n],sample_size, axis=0).reshape(sample_size, n_features)
             w_samples[w_n] = w_sample
 
         datasets_next = []
-        count=0
-        for X_i in datasets_current:
-            X_i_next = X_i - w_samples[count]
+        for i, X_i in enumerate(datasets_current):
+            X_i_next = X_i - w_samples[i]
             datasets_next.append(X_i_next.tolist())
-            count+=1
             
         weights.append(w)      
-            
-        k += 1
 
-    w_final = np.zeros((len(datasets),len(datasets[0][0]),K))
+    w_final = np.zeros((len(datasets), n_features, K))
     for k, w_k in enumerate(weights):
         #print(f"k: {k}")
         for n, w_value in enumerate(w_k.values()):
@@ -158,9 +163,6 @@ def lp_pmd(datasets:list, penalties:list,  K:int, standardize:bool, mimic_R):
                 #print(w_feature)
                 w_final[n][f][k] = w_feature
         
-    w_init = [np.full((len(datasets[0][0]),K), 0.5)]*len(datasets)
+    w_init = [np.full((n_features, K), 0.5)]*len(datasets)
     return w_final, w_init
-
-def multicca(datasets:list, penalties:list,  K:int, standardize, mimic_R):
-    return lp_pmd(datasets, penalties, K, standardize, mimic_R)
     
